4da0175b1960cf74fa5189c318d73b073b0bee80
Frederik Pietzko <frederik.pietzko@iits-consulting.de>
2025-07-08 11:54:34 +0200

    i can parse hello world now

--- rust/http_server/src/main.rs
+++ rust/http_server/src/main.rs
@@ -1,13 +1,13 @@
 mod prelude;
-mod websocket_upgrade;
+mod upgrade;
 
 use bytecodec::DecodeExt;
 use env_logger::Builder;
 use httpcodec::{HttpVersion, Method, ReasonPhrase, Request, RequestDecoder, Response, StatusCode};
 use prelude::*;
 use std::io::{Read, Write};
+use upgrade::UpgradeResponseHandler;
 use wasmedge_wasi_socket::{TcpListener, TcpStream};
-use websocket_upgrade::UpgradeResponseHandler;
 
 fn handle_http(req: Request<String>) -> Result<Response<String>> {
     info!(
@@ -89,6 +89,30 @@ fn handle_client(mut stream: TcpStream) -> Result<()> {
     Ok(())
 }
 
+#[derive(Debug)]
+enum OpCode {
+    Continuation = 0x0,
+    Text = 0x1,
+    Binary = 0x2,
+    Close = 0x8,
+    Ping = 0x9,
+    Pong = 0xA,
+}
+
+impl From<u8> for OpCode {
+    fn from(byte: u8) -> Self {
+        match byte {
+            0x0 => OpCode::Continuation,
+            0x1 => OpCode::Text,
+            0x2 => OpCode::Binary,
+            0x8 => OpCode::Close,
+            0x9 => OpCode::Ping,
+            0xA => OpCode::Pong,
+            _ => panic!("Unknown opcode: {}", byte),
+        }
+    }
+}
+
 fn websocket_message_loop(mut stream: TcpStream) -> Result<()> {
     let mut buffer = [0u8; 4096];
 
@@ -99,13 +123,33 @@ fn websocket_message_loop(mut stream: TcpStream) -> Result<()> {
                 return Ok(());
             }
             Ok(bytes_read) => {
+                info!("Read {} bytes", bytes_read);
+                info!("Buffer: {:?}", &buffer[..bytes_read]);
+                let buffer = &buffer[..bytes_read];
+                let first_byte = &buffer[0..1];
+                let opcode: OpCode = (first_byte[0] & 0x0F).into();
+                let second_byte = &buffer[1..2];
+                let masked = (second_byte[0] & 0x80) != 0;
+                let payload_len_1 = (second_byte[0] & 0x7F) as usize;
+                let mask = if masked { &buffer[2..6] } else { &[] };
+                let payload = &buffer[6..bytes_read];
+                let unmasked_payload: Vec<u8> = if masked {
+                    payload
+                        .iter()
+                        .zip(mask.iter().cycle())
+                        .map(|(p, m)| p ^ m)
+                        .collect()
+                } else {
+                    payload.to_vec()
+                };
+                let payload = unmasked_payload
+                    .iter()
+                    .map(|c| *c as char)
+                    .collect::<String>();
                 info!(
-                    "Received {} bytes: {:02x?}",
-                    bytes_read,
-                    &buffer[..bytes_read]
-                );
-                let first_byte = &buffer[..1];
-                info!("Received first byte: {:0b}", first_byte[0]);
+                    "Received WebSocket message: opcode={:?}, masked={}, payload_len={}, mask={:?}, payload={}",
+                    opcode, masked, payload_len_1, mask, payload
+                )
             }
             Err(e) => {
                 error!("Error reading from stream: {}", e);
