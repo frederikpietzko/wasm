aaca8b925ce38a4fde640bacdafe6988534a0d70
Frederik Pietzko <frederik.pietzko@iits-consulting.de>
2025-07-20 16:32:29 +0200

    renmae to ws_server

--- rust/ws_server/build-image.sh
+++ rust/ws_server/build-image.sh
@@ -6,4 +6,4 @@ podman build \
   -t frederikpietzko/wasm-http-server:latest \
   .
 
-podman push frederikpietzko/wasm-http-server:latest docker://docker.io/frederikpietzko/wasm-http-server:latest
+podman push frederikpietzko/wasm-ws-server:latest docker://docker.io/frederikpietzko/wasm-ws-server:latest
