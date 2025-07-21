cf087ac194970524631395e679db74d8fa613193
Frederik Pietzko <frederik.pietzko@iits-consulting.de>
2025-07-21 14:28:36 +0200

    move things around and cleanup

--- rust/http_server/build-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-podman build \
-  --platform=wasm/wasi \
-  --annotation "module.wasm.image/variant=compat" \
-  -t frederikpietzko/http-server:latest \
-  .
-
-podman push frederikpietzko/http-server:latest docker://docker.io/frederikpietzko/http-server:latest
--- rust/http_server_normal/build-image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-podman build \
-  -t frederikpietzko/http-server-normal:latest \
-  .
-
-podman push frederikpietzko/http-server-normal:latest docker://docker.io/frederikpietzko/http-server-normal:latest
